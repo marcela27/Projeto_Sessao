b447ac686f31da18b85784a52b76764b90667c51
marcela27 <marcela.almeida17@etec.sp.gov.br>
2022-06-02 16:00:58 -0300

    projeto_sessao

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Sessão PHP</title>
+    </head>
+    <body>
+      <form method="poost" action="ope.php" id="formlogin">
+          <label>NOME: </label>
+          <input type="text" name="login" id="login"><br><br>
+          <label>SENHA: </label>
+          <input type="password" name="senha" id="senha"><br><br>
+          <input type="submit" value="LOGAR">
+      </form>
+    </body>
+</html>
